78419f34d331d08603d80ed193616b9738c7e8b1
JaeHyeok Choi <77189999+jjaegii@users.noreply.github.com>
2022-07-02 19:35:22 +0900

    FEAT userEntity

---
+++ src/models/userEntity.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  socialId: { type: String, required: true, unique: true },
+  socialType: { type: String, required: true },
+  socialNickname: { type: String },
+  nickname: { type: String, default: "닉네임" },
+  email: { type: String },
+  posting: [{ type: mongoose.Schema.Types.ObjectId, ref: "Posting" }],
+});
+
+const User = mongoose.model("user", userSchema);
+export default User;
