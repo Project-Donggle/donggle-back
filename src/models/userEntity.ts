5ff4fc49dd406dcde5f2565158aec4f36dd0159e
oddcode <oddcode@naver.com>
2022-07-06 00:34:55 +0900

    ENH userEntity 수정 및 보완

--- src/models/userEntity.ts
+++ src/models/userEntity.ts
@@ -1,12 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { now } from "mongoose";
+import { boolean } from "webidl-conversions";
 
 const userSchema = new mongoose.Schema({
-  socialId: { type: String, required: true, unique: true },
-  socialType: { type: String, required: true },
-  socialNickname: { type: String },
-  nickname: { type: String, default: "닉네임" },
-  email: { type: String },
-  posting: [{ type: mongoose.Schema.Types.ObjectId, ref: "Posting" }],
+  social: {
+    id: { type: String, required: true, unique: true },
+    sort: { type: "naver" || "kakao" || "notSocial", required: true },
+  },
+  nickname: { type: String, default: "동글이", minLength: 2 },
+  email: { type: String, required: true },
+  board: {
+    write: [{ type: mongoose.Schema.Types.ObjectId, ref: "board" }],
+    liked: [{ type: mongoose.Schema.Types.ObjectId, ref: "board" }],
+  },
+  comment: {
+    liked: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
+    write: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
+  },
+  location: {
+    fixed: [
+      { alias: { type: String, default: "집" }, coord: [Number] },
+      { alias: { type: String, default: "회사" }, coord: [Number] },
+      { alias: { type: String, default: "학교" }, coord: [Number] },
+    ],
+    now: [Number, Number],
+  },
+  agree: {
+    ad: { type: Date },
+    privacy: { type: Date },
+  },
 });
 
 const user = mongoose.model("user", userSchema);
